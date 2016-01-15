1aae0cfaa8d99d49fab766507a824ed02784f26c
Priyanka Saigal <email": null>
2016-01-14 17:50:54 -0800

    Functioning calculator. Numbers display on browser. Added css. Still need to refactor code.

--- calculator.js
+++ calculator.js
@@ -2,35 +2,43 @@ var storeData = {
   data : []
 }
 
-function displayNumber() {
-  var array = []
+function retrieveNumber() {
   var e = event.target; //event is the click event(target is property of event, which refers to HTML element that was clicked)
-  var number = e.innerHTML
-  storeNumbers(number)
+  var number = e.innerHTML //represents the number that was clicked
+  if (number == "+" || number == "-" || number == "/" || number == "*" || number == "AC" ) {
+    document.getElementById("display").innerHTML = "";
+  }
+  else if (number != "=") {
+     document.getElementById("display").innerHTML += number;
+  }
+   storeNumbers(number);
 }
 
 function storeNumbers(number) {
-  if (number != "=") {
+  if (number != "=" && number != "AC") {
     storeData.data.push(number);
+  }
+  else if (number == "AC") {
+    storeData.data = [];
   }
   else {
     processNumbers(storeData.data)
-    storeData.data = [];
   }
 }
+
 
 function processNumbers(array) {
   for (i = 0; i < array.length; i ++) {
     if (array[i] == "+" || array[i] == "-" || array[i] == "/" || array[i] == "*") {
       var num1 = parseInt(array.slice(0,i).join("").replace(/,/g, ''));
       var num2 = parseInt(array.slice(i+1, array.length).join("").replace(/,/g, ''));
-      if (array[i] == "+"){
+      if (array[i] == "+") {
         add(num1,num2);
       }
-      else if (array[i] == "-"){
+      else if (array[i] == "-") {
         subtract(num1,num2);
       }
-      else if (array[i] == "/"){
+      else if (array[i] == "/") {
         divide(num1,num2);
       }
       else {
@@ -41,17 +49,22 @@ function processNumbers(array) {
 }
 
 function add(num1, num2) {
-  console.log(num1 + num2);
+  storeData.data = [(num1 + num2).toString()]
+  document.getElementById("display").innerHTML = storeData.data;
 }
 
 function subtract(num1, num2) {
-  console.log(num1 - num2);
+  storeData.data = [(num1 - num2).toString()]
+  document.getElementById("display").innerHTML = storeData.data;
 }
 
 function multiply(num1, num2) {
-  console.log(num1 * num2);
+  storeData.data = [(num1 * num2).toString()]
+  document.getElementById("display").innerHTML = storeData.data;
 }
 
 function divide(num1, num2) {
-  console.log(num1 / num2);
+  storeData.data = [(num1 / num2).toString()];
+  document.getElementById("display").innerHTML = storeData.data;
 }
+
